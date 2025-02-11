ad8784e0ae56b72d896bc2927897b4b25f92f418
chaoui-ahmed <tahachaoui5@gmail.com>
2025-02-11 15:28:38 +0100

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,10 @@
+import HeroResume from "@/components/hero-resume"
+
+export default function Page() {
+  return (
+    <div className="min-h-screen bg-[#030303]">
+      <HeroResume />
+    </div>
+  )
+}
+
